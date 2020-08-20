040fde3873700ea9c6ac2448e540d27c6cad453d
Aidan Dang <aidandang@gmail.com>
2020-08-19 19:05:36 -0700

    added node server settings and tested server working

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const errorController = require('../controllers/errorController');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// the root route or no routes found
+server.use('/', (req, res) => {
+  res.json({ message: 'Build Week: Expat Journal 1.' })
+});
+
+// global error controller
+server.use("/", errorController);
+
+module.exports = server;
