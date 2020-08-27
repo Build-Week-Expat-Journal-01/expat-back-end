a0c94e1b471c22e3631be4164b11853882b97bfb
Aidan Dang <aidandang@gmail.com>
2020-08-27 07:19:01 -0700

    fixed knexfile for production environment

--- knexfile.js
+++ knexfile.js
@@ -15,17 +15,9 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'ddp887n5epdnbe',
-      user:     'ttzthvqclsqydf',
-      password: '428e1d885ac9d2ae32e9e8ee3f2b350bd2f9a6f7eea3df0586226cbd0f0be142'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/data/migrations'
     }
   }
 };
