bce1e727939c37cd732c68617b1304d3134e801a
Aidan Dang <aidandang@gmail.com>
2020-08-26 20:07:50 -0700

    created production config for data migration to Postgres

--- knexfile.js
+++ knexfile.js
@@ -12,5 +12,20 @@ module.exports = {
     useNullAsDefault: true,
     migrations: { directory: './data/migrations' },
     seeds: { directory: './data/seeds' }
+  },
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'ddp887n5epdnbe',
+      user:     'ttzthvqclsqydf',
+      password: '428e1d885ac9d2ae32e9e8ee3f2b350bd2f9a6f7eea3df0586226cbd0f0be142'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 };
